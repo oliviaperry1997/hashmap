0bad4a42e9e0acb64ae45529fba846a74ea3e3ce
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-06-09 15:22:40 +0100

    Add hashmap class with test file and module import
    This commit adds the basic HashMap class, a test file, and an imported
    class LinkedList which i made in a previous project.

---
+++ hashmap.js
@@ -0,0 +1,60 @@
+import { LinkedList } from "./linkedlist.js";
+
+export class HashMap {
+    constructor() {
+        this.loadFactor = 0.75;
+        this.capacity = 16;
+        this.size = 0;
+        this.buckets = Array.from({ length: this.capacity }, () => new LinkedList());
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode =
+                (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        const before = bucket.size();
+        bucket.replaceOrAppend(key, value);
+        const after = bucket.size();
+
+        if (after > before) {
+            this.size++;
+        }
+
+        if (this.size / this.capacity > this.loadFactor) {
+            this.resize();
+        }
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        const node = this.buckets[index].findByKey(key);
+        return node ? node.value.value : undefined;
+    }
+
+    resize() {
+        const oldBuckets = this.buckets;
+        this.capacity *= 2;
+        this.buckets = Array.from({ length: this.capacity }, () => new LinkedList());
+        this.size = 0;
+
+        for (const bucket of oldBuckets) {
+            let node = bucket.head;
+            while (node) {
+                this.set(node.value.key, node.value.value);
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ linkedlist.js
@@ -0,0 +1,162 @@
+export class LinkedList {
+    constructor() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.length++;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        if (this.head) {
+            newNode.next = this.head;
+        }
+        this.head = newNode;
+        this.length++;
+    }
+
+    size() {
+        return this.length;
+    }
+
+    printHead() {
+        return this.head;
+    }
+
+    printTail() {
+        if (!this.head) {
+            return null;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            return current;
+        }
+    }
+
+    at(index) {
+        let current = this.head;
+        for (let i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    pop() {
+        if (this.head) {
+            let current = this.head;
+            while (current.next.next) {
+                current = current.next;
+            }
+            current.next = null;
+        }
+        this.length--;
+    }
+
+    contains(value) {
+        let current = this.head;
+        while (current) {
+            if (current.value === value) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    find(value) {
+        let current = this.head;
+        let index = 0;
+        while (current) {
+            if (current.value === value) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return null;
+    }
+
+    toString() {
+        let current = this.head;
+        let output = "";
+        while (current) {
+            output +=
+                `( ${current.value} )` + (current.next ? " -> " : " -> null");
+            current = current.next;
+        }
+        return output;
+    }
+
+    insertAt(value, index) {
+        const newNode = new Node(value)
+        if (index === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            for (let i=0; i<(index-1) && i<this.length; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        this.length++
+    }
+
+    removeAt(index) {
+        if (index === 0) {
+            this.head = this.head.next;
+        } else if (index >= this.length) {
+            return;
+        } else {
+            let current = this.head;
+            for (let i=0; i<(index-1); i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        this.length--;
+    }
+
+    findByKey(key) {
+        let current = this.head;
+        while (current) {
+            if (current.value.key === key) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    replaceOrAppend(key, value) {
+        const existingNode = this.findByKey(key);
+        if (existingNode) {
+            existingNode.value.value = value;
+        } else {
+            this.append({ key, value });
+            this.length++;
+        }
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
